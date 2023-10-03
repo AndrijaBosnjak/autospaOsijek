af8507e8924a04d724ca1fddab98755b1caee611
AndrijaBosnjak <andrijab2@gmail.com>
2023-10-03 12:09:52 +0200

    add app header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <main></main>
+      <Footer></Footer>
     </div>
   );
 }
